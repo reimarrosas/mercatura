f59307834a31328ddae868dd9a88b71f9d63da54
Reimar Rosas <reimar@reimarrosas.me>
2023-05-05 18:00:42 -0400

    Cleanup/API: Remove unnecessary stuff in tests and controllers (#4)
    * Cleanup/API/Types: removed all the types in the tests and used
    `docker-compose` instead of `docker compose`
    
    * Cleanup/API/Tests: Merged repetitive test definitions into `it.each`
    
    * Cleanup/API/Controllers: Removed the unnecessary null properties in the
    response body

--- api/src/controllers/auth.int.test.ts
+++ api/src/controllers/auth.int.test.ts
@@ -1,15 +1,9 @@
 import { faker } from '@faker-js/faker'
 
-import { SignupCredentials } from "../../types"
 import supertest from 'supertest';
 import app from '../app';
 import { prisma } from '../database';
 import { redisClient } from '../utils/config';
-
-type Response = {
-    message?: string;
-    error?: string;
-}
 
 describe('Authentication Integration', () => {
     const initialUser = {
@@ -18,7 +12,15 @@ describe('Authentication Integration', () => {
         password: '$argon2id$v=19$m=16,t=3,p=1$aFo2ZkdIUEVyd0x1UW5lTA$y7ojp7qh6Sa2Iv2lKsErwDjJkmwuexT2EWMiPg5Yu8Y'
     }
 
-    const generatePassword = () => 'Aa1' + faker.internet.password(8)
+    const generateSinglePassword = () => 'Aa1' + faker.internet.password(8)
+
+    const generateBothPasswords = () => {
+        const pw = generateSinglePassword()
+        return {
+            password: pw,
+            confirmPassword: pw
+        }
+    }
 
     beforeAll(async () => {
         await prisma.user.create({
@@ -37,113 +39,42 @@ describe('Authentication Integration', () => {
     })
 
     describe('Signup', () => {
+        const validSignupCredentials = {
+            name: faker.name.fullName(),
+            email: faker.internet.email(),
+            ...(generateBothPasswords())
+        }
+
         it('should return 201 on valid signup credentials', async () => {
-            const password = generatePassword()
-            const signupCreds: SignupCredentials = {
-                name: faker.name.fullName(),
-                email: faker.internet.email(),
-                password,
-                confirmPassword: password
-            }
+            const response = await supertest(app).post('/api/v1/signup').send(validSignupCredentials)
 
-            const response = await supertest(app).post('/api/v1/signup').send(signupCreds)
-
-            expect<number>(response.statusCode).toBe(201)
-            expect<Response>(response.body).toEqual({
+            expect(response.statusCode).toBe(201)
+            expect(response.body).toEqual({
                 message: 'User signup successful',
-                error: null
             })
 
             const insertedUser = await prisma.user.findFirst({
-                where: { email: signupCreds.email }
+                where: { email: validSignupCredentials.email }
             })
 
             expect(insertedUser).not.toBeNull()
-            expect(insertedUser?.name).toBe(signupCreds.name)
-            expect(insertedUser?.password).not.toBe(signupCreds.password)
+            expect(insertedUser?.name).toBe(validSignupCredentials.name)
+            expect(insertedUser?.password).not.toBe(validSignupCredentials.password)
             expect(insertedUser?.password.startsWith('$argon2id')).toBe(true)
         })
 
-        it('should return 422 on wrong POST body schema', async () => {
-            const response = await supertest(app).post('/api/v1/signup').send({})
+        it.each([
+            [422, 'wrong POST body schema', { error: 'Body should contain: name|string, email|string, password|string, confirmPassword|string' }, {}],
+            [422, 'different passwords', { error: 'Password does not match' }, { ...validSignupCredentials, confirmPassword: generateSinglePassword()}],
+            [422, 'invalid email pattern', { error: 'Invalid email' }, { ...validSignupCredentials, email: 'test.com' }],
+            [422, 'invalid password pattern', { error: 'Invalid password' }, { ...validSignupCredentials, password: 'Pw1', confirmPassword: 'Pw1' }],
+            [403, 'already existing email', { error: 'User already exists' }, { ...validSignupCredentials, email: 'sample@test.com' }]
+        ])('should return status %i on %s', async (statusCode, _description, body, credentials) => {
+            const response = await supertest(app).post('/api/v1/signup').send(credentials)
 
-            expect<number>(response.statusCode).toBe(422)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'Body should contain: name|string, email|string, password|string, confirmPassword|string'
-            })
+            expect(response.statusCode).toBe(statusCode)
+            expect(response.body).toEqual(body)
         })
-
-        it('should return 422 on different password', async () => {
-            const signupCreds: SignupCredentials = {
-                name: faker.name.fullName(),
-                email: faker.internet.email(),
-                password: generatePassword(),
-                confirmPassword: generatePassword()
-            }
-
-            const response = await supertest(app).post('/api/v1/signup').send(signupCreds)
-
-            expect<number>(response.statusCode).toBe(422)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'Password does not match'
-            })
-        })
-
-        it('should return 422 on invalid email pattern', async () => {
-            const password = generatePassword()
-            const signupCreds: SignupCredentials = {
-                name: faker.name.fullName(),
-                email: faker.name.fullName(),
-                password,
-                confirmPassword: password
-            }
-
-            const response = await supertest(app).post('/api/v1/signup').send(signupCreds)
-
-            expect<number>(response.statusCode).toBe(422)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'Invalid email'
-            })
-        })
-
-        it('should return 422 on invalid password pattern', async () => {
-            const password = faker.lorem.word(4)
-            const signupCreds: SignupCredentials = {
-                name: faker.name.fullName(),
-                email: faker.internet.email(),
-                password: 'Pw1',
-                confirmPassword: password
-            }
-
-            const response = await supertest(app).post('/api/v1/signup').send(signupCreds)
-
-            expect<number>(response.statusCode).toBe(422)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'Invalid password'
-            })
-        })
-
-        it('should return 403 on already existing email', async () => {
-            const signupCreds: SignupCredentials = {
-                name: 'Sample User',
-                email: 'sample@test.com',
-                password: 'Sample-test123',
-                confirmPassword: 'Sample-test123'
-            }
-
-            const response = await supertest(app).post('/api/v1/signup').send(signupCreds)
-
-            expect<number>(response.statusCode).toBe(403)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'User already exists'
-            })
-        })
-
     })
 
     describe('Login', () => {
@@ -159,86 +90,21 @@ describe('Authentication Integration', () => {
             expect(response.headers['set-cookie']).not.toBeFalsy()
             expect(response.body).toEqual({
                 message: 'User login successful',
-                error: null
             })
         })
 
-        it('should return 403 if user does not exist', async () => {
-            const loginCreds = {
-                email: 'sample-ne@test.com',
-                password: 'Sample-test123'
-            }
+        it.each([
+            [403, 'user not existing', { error: 'User does not exist' }, { email: 'sample-ne@test.com', password: 'Sample-test123' }],
+            [401, 'password provided not matching', { error: 'Invalid password' }, { email: 'sample@test.com', password: 'WrongPassw0rd' }],
+            [422, 'wrong POST body schema', { error: 'Body should contain: email|string, password|string' }, { email: undefined, password: null }],
+            [422, 'invalid email pattern', { error: 'Invalid email' }, { email: 'test.com', password: 'Sample-test123' }],
+            [422, 'invalid password pattern', { error: 'Invalid password' }, { email: 'sample@test.com', password: 'Pw1' }]
+        ])('should return %i on %s', async (statusCode, _description, body, credentials) => {
+            const response = await supertest(app).post('/api/v1/login').send(credentials)
 
-            const response = await supertest(app).post('/api/v1/login').send(loginCreds)
-
-            expect(response.statusCode).toBe(403)
+            expect(response.statusCode).toBe(statusCode)
             expect(response.headers['set-cookie']).toBeFalsy()
-            expect(response.body).toEqual({
-                message: null,
-                error: 'User does not exist'
-            })
-        })
-
-        it('should return 401 if password provided does not match', async () => {
-            const loginCreds = {
-                email: 'sample@test.com',
-                password: 'WrongPassw0rd'
-            }
-
-            const response = await supertest(app).post('/api/v1/login').send(loginCreds)
-
-            expect(response.statusCode).toBe(401)
-            expect(response.headers['set-cookie']).toBeFalsy()
-            expect(response.body).toEqual({
-                message: null,
-                error: 'Invalid password'
-            })
-        })
-
-        it('should return 422 on wrong POST body schema', async () => {
-            const loginCreds = {
-                email: undefined,
-                password: null
-            }
-
-            const response = await supertest(app).post('/api/v1/login').send(loginCreds)
-
-            expect(response.statusCode).toBe(422)
-            expect(response.headers['set-cookie']).toBeFalsy()
-            expect(response.body).toEqual({
-                message: null,
-                error: 'Body should contain: email|string, password|string'
-            })
-        })
-
-        it('should return 422 on invalid email pattern', async () => {
-            const loginCreds = {
-                email: 'test.com',
-                password: 'Sample-test123'
-            }
-
-            const response = await supertest(app).post('/api/v1/login').send(loginCreds)
-
-            expect<number>(response.statusCode).toBe(422)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'Invalid email'
-            })
-        })
-
-        it('should return 422 on invalid password pattern', async () => {
-            const loginCreds = {
-                email: 'sample@test.com',
-                password: 'Pw1'
-            }
-
-            const response = await supertest(app).post('/api/v1/login').send(loginCreds)
-
-            expect<number>(response.statusCode).toBe(422)
-            expect<Response>(response.body).toEqual({
-                message: null,
-                error: 'Invalid password'
-            })
+            expect(response.body).toEqual(body)
         })
     })
 })
